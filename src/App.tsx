02b28e90c6f36936a968228903cbaf79e8ba23cc
joshuarichard <eos.josh.richard@gmail.com>
2020-12-13 11:06:55 -0500

    third commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from './style/components';
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </Header>
+    </div>
+  );
+}
+
+export default App;
